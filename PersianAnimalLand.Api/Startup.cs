69dea60873d2a02a39f8b0b3fb9db933b4a37383
Navid Mpr <navid_mon@yahoo.com>
2020-08-31 16:56:28 +0430

    change

--- PersianAnimalLand.Api/Startup.cs
+++ PersianAnimalLand.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,13 +13,16 @@ namespace PersianAnimalLand.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _config { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ PersianAnimalLand.DependencyResolver/Ioc/IocContainerExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PersianAnimalLand.DependencyResolver.Ioc
+{
+    public static class IocContainerExtension
+    {
+        public static IServiceCollection AddTransient(this IServiceCollection serviceCollection, IConfiguration _configuration)
+        {
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddTransient()
+        {
+
+        }
+        public static IServiceCollection AddSingleton()
+        {
+
+        }
+    }
+}
